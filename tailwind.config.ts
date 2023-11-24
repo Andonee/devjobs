3c496be5f639b0c44d7ab07dda52b32af2c29d1e
Andonee <mateusz.poprawa@gmail.com>
2023-11-24 15:40:18 +0100

    add-tailwind-theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		colors: {
+			violet: '#9e7f66',
+			lightViolet: '#939BF4',
+			darkBlue: '#19202D',
+			midnight: '#121721',
+			snowy: '#ffffff',
+			lightGrey: '#F4F6F8',
+			grey: '#9DAEC2',
+			darkGrey: '#6E8098',
+		},
+	},
+	plugins: [],
+};
+export default config;
