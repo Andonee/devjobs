8e5f7488107f081006be71b32581415ca0cb0d89
Andonee <mateusz.poprawa@gmail.com>
2023-12-04 08:58:14 +0100

    add basic navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-"use client";
+import { Navbar } from "@/components";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis harum
+        deserunt impedit nesciunt at vel, ab iste mollitia a soluta ipsa tempore
+        in officiis voluptatem, enim dicta beatae facilis rem.
+      </div>
+    </main>
+  );
 }
---
+++ components/navbar/logo/Logo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Logo = () => {
+  return (
+    <div className="h-fit">
+      <h1 className="font-bold text-snowy">devjob</h1>
+    </div>
+  );
+};
